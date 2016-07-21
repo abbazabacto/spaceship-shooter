5b3908403a5f128be502861c5eaf09e192cde34e
abbazabacto <abbazabacto@gmail.com>
2016-07-22 00:20:09 +0200

    fix: use absolute urls for threex.planets baseurl

--- src/actors/planets.js
+++ src/actors/planets.js
@@ -2,7 +2,7 @@ import THREE from 'three';
 import Rx from 'rx';
 
 import THREEx from '../../lib/planets/threex.planets';
-THREEx.Planets.baseURL = '../lib/planets/';
+THREEx.Planets.baseURL = '/lib/planets/';
 
 import _, { createAtmosphereMaterial } from '../../lib/planets/threex.atmospherematerial';
 
@@ -13,7 +13,6 @@ const mesh = THREEx.Planets.createEarth(2000);
 mesh.position.y = -2400;
 mesh.rotation.x = getRad(90);
 mesh.rotation.y = getRad(90);
-console.log(mesh);
 scene.add(mesh);
 
 export const earth$ = Rx.Observable.of(mesh);
