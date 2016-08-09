4a17de6b6b89e694a78eb3a31dc2a82d251d72f0
abbazabacto <abbazabacto@gmail.com>
2016-08-09 12:27:04 +0200

    fix: new attempt on auto gh-pages deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,11 +2,25 @@
 
 set -o errexit # Exit on error
 
+message_info () {
+  echo "${textGreen}[spaceshipshooter-app-deploy]${textReset} $1"
+}
+
+message_info "git pull -r"
 git pull -r
+message_info "git push origin master"
 git push origin master
+message_info "git checkout gh-pages"
 git checkout gh-pages
+message_info "git rebase master"
 git rebase master
-git commit -a -m 'feat: update to latest master'
+message_info "git pull -r"
 git pull -r
-git push origin gh-pages
-git checkout master
+message_info "npm run build"
+npm run build
+# message_info "git commit -a -m 'feat: update to latest master'"
+# git commit -a -m 'feat: update to latest master'
+# message_info "git push origin gh-pages"
+# git push origin gh-pages
+# message_info "git checkout master"
+# git checkout master
