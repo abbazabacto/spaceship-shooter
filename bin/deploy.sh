9487051bdb46ba4fc892c47420c9a9537b2451dd
abbazabacto <abbazabacto@gmail.com>
2016-08-09 12:36:55 +0200

    fix: enable commit and push in deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,19 +8,27 @@ message_info () {
 
 message_info "git pull -r"
 git pull -r
+
 message_info "git push origin master"
 git push origin master
+
 message_info "git checkout gh-pages"
 git checkout gh-pages
+
 message_info "git rebase master"
 git rebase master
+
 message_info "git pull -r"
 git pull -r
+
 message_info "npm run build"
 npm run build
-# message_info "git commit -a -m 'feat: update to latest master'"
-# git commit -a -m 'feat: update to latest master'
-# message_info "git push origin gh-pages"
-# git push origin gh-pages
-# message_info "git checkout master"
-# git checkout master
+
+message_info "git commit -a -m 'feat: update to latest master'"
+git commit -a -m 'feat: update to latest master'
+
+message_info "git push origin gh-pages"
+git push origin gh-pages
+
+message_info "git checkout master"
+git checkout master
