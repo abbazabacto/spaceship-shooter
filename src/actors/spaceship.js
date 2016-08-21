086c8bcbfffb93fc26519e65103770be75475f93
abbazabacto <abbazabacto@gmail.com>
2016-08-21 17:52:36 +0200

    feat: spaceship parts draft implementation

--- src/actors/spaceship.js
+++ src/actors/spaceship.js
@@ -1,19 +1,65 @@
 import THREE from 'three';
 import Rx from 'rx';
 
-const spaceshipLoad$ = new Rx.ReplaySubject(1);
+import { createModel } from '../utils/models';
+import { getRad } from '../utils/misc';
 
-const loader = new THREE.JSONLoader();
+const ringsModel = createModel('res/models/spaceship-parts/rings.json');
+const gunsModel = createModel('res/models/spaceship-parts/guns.json');
+const shipLowerModel = createModel('res/models/spaceship-parts/ship-lower.json');
+const chairModel = createModel('res/models/spaceship-parts/chair.json');
+const controlBoardModel = createModel('res/models/spaceship-parts/control-board.json');
 
-loader.load('res/models/spaceship.json', function (geometry, materials) {
-  spaceshipLoad$.onNext({geometry, materials});
+// add to preloads$...
+/// spaceshipModel.progress$.subscribe(console.log.bind(console));
+
+// utils/models.js -> createGroup?
+export const spaceshipObject$ = Rx.Observable.combineLatest(
+  ringsModel.object$,
+  gunsModel.object$,
+  shipLowerModel.object$,
+  chairModel.object$,
+  controlBoardModel.object$,
+  (
+    rings,
+    guns,
+    shipLower,
+    chair,
+    controlBoard
+  ) => ({
+    rings,
+    guns,
+    shipLower,
+    chair,
+    controlBoard,
+  })
+)
+.map(({ 
+  rings,
+  guns,
+  shipLower,
+  chair,
+  controlBoard,
+}) => {
+  const _frame = new THREE.Object3D();
+  const frame = new THREE.Object3D();
+  frame.add(rings);
+  frame.add(guns);
+  frame.add(controlBoard);
+  frame.rotation.y = getRad(-90);
+  _frame.add(frame);
+
+  const _base = new THREE.Object3D();
+  const base = new THREE.Object3D();
+  base.add(shipLower);
+  base.add(chair);
+  base.add(controlBoard);
+  base.rotation.y = getRad(-90);
+  _base.add(base);
+
+  const spaceship = new THREE.Object3D();
+  spaceship.add(_frame);
+  spaceship.add(_base);
+
+  return spaceship;
 });
-
-export const spaceshipMesh$ = spaceshipLoad$
-  .map(function({geometry, materials}){
-    const mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-    mesh.position.y -= 1;
-    mesh.scale.set(2, 2, 2);
-    
-    return mesh;
-  });
