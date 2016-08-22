ab20b67c1cd4757405f8454af6e788602b4513db
abbazabacto <abbazabacto@gmail.com>
2016-08-22 16:24:25 +0200

    feat: add gameLoop tool

---
+++ src/tools/gameLoop.js
@@ -0,0 +1,36 @@
+import Rx from 'rx';
+
+import { animationFrame$ } from './animationFrame';
+
+
+export const createGameLoop = (actors) => {
+  const actorsKeysList = Object.keys(actors);
+  const actorsList = actorsKeysList.map(key => actors[key]);
+
+  return Rx.Observable.combineLatest(
+    animationFrame$,
+    ...actorsList,
+    createActorCombiner([
+      'animationFrame',
+      ...actorsKeysList,
+    ])
+  )
+  //only update when frame is ready to render
+  //otherwise frames could drop on overloading changes in other observables
+  .distinctUntilChanged(({ animationFrame }) => animationFrame.timestamp);
+}
+
+function createActorCombiner(keys) {
+  const strippedKeys = keys.map(key => key.replace(/\$$/, ''));
+
+  return function actorCombiner() {
+    const args = [...arguments];
+    const actors = {};
+
+    args.forEach((arg, index) => {
+      actors[strippedKeys[index]] = arg;
+    });
+
+    return actors;
+  }
+}
