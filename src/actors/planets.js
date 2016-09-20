42b2dbd5dad28a9ca6fabd3efbc3bec9c3485c62
abbazabacto <abbazabacto@gmail.com>
2016-09-20 16:44:56 +0300

    fix: clean up planets variable names

--- src/actors/planets.js
+++ src/actors/planets.js
@@ -20,16 +20,16 @@ containerEarth.position.y = -3400;
 containerEarth.position.z = -20;
 scene.add(containerEarth);
 
-var geometry	= new THREE.SphereGeometry(3000 / 5, 16, 16)
-var material	= new THREE.MeshPhongMaterial({
+const moonGeometry = new THREE.SphereGeometry(3000 / 5, 16, 16);
+const moonMaterial = new THREE.MeshPhongMaterial({
   map: loader.load('dist/res/models/planets/moon/moon-col.jpg'),
   normalMap: loader.load('dist/res/models/planets/moon/moon-nrm.jpg'),
   normalScale: new THREE.Vector2(3,3),
   emissive: new THREE.Color(0.12, 0.12, 0.12),
   emissiveIntensity: 1,
-})
+});
 
-var moonMesh = new THREE.Mesh(geometry, material)
+const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
 moonMesh.position.z = -8000;
 moonMesh.position.y = -1500;
 moonMesh.rotation.x = getRad(90);
@@ -38,8 +38,8 @@ moonMesh.receiveShadow = true;
 moonMesh.castShadow	= true;
 containerEarth.add(moonMesh);
 
-var geometry	= new THREE.SphereGeometry(3000, 64, 64)
-var material	= new THREE.MeshPhongMaterial({
+const earthGeometry = new THREE.SphereGeometry(3000, 64, 64);
+const earthMaterial	= new THREE.MeshPhongMaterial({
   map	: loader.load('dist/res/models/planets/earth/earth-night.png'),
   normalMap	: loader.load('dist/res/models/planets/earth/earth-normal.png'),
   normalScale: new THREE.Vector2(0.384084,0.384084),
@@ -51,21 +51,21 @@ var material	= new THREE.MeshPhongMaterial({
   emissiveIntensity: 0.5,
 });
 
-var earthMesh = new THREE.Mesh(geometry, material);
+const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
 earthMesh.rotation.x = getRad(-30);
 earthMesh.rotation.z = getRad(40);
 earthMesh.receiveShadow = true;
 earthMesh.castShadow = true;
 containerEarth.add(earthMesh);
 
-var geometry	= new THREE.SphereGeometry(3000, 64, 64)
-var material	= new THREE.MeshBasicMaterial({
+const earthCloudsGeometry = new THREE.SphereGeometry(3000, 64, 64);
+const earthCloudsMaterial = new THREE.MeshBasicMaterial({
   map: loader.load('dist/res/models/planets/earth/earth-clouds-darker.png'),
   blending: THREE.AdditiveBlending,
   transparent: true,
-})
+});
 
-var earthCloudsMesh = new THREE.Mesh(geometry, material)
+const earthCloudsMesh = new THREE.Mesh(earthCloudsGeometry, earthCloudsMaterial);
 earthCloudsMesh.scale.multiplyScalar(1.0025);
 earthCloudsMesh.rotation.x = getRad(-280);
 earthCloudsMesh.rotation.y = getRad(45);
@@ -74,27 +74,26 @@ earthCloudsMesh.receiveShadow = true;
 earthCloudsMesh.castShadow	= true;
 containerEarth.add(earthCloudsMesh);
 
-var geometry = new THREE.SphereGeometry(3000, 64, 64);
-var material = createAtmosphereMaterial();
-material.uniforms.glowColor.value.set(0x00b3ff);
-material.uniforms.coeficient.value = 0.8;
-material.uniforms.power.value = 2.0;
+const atmosphereGeometry = new THREE.SphereGeometry(3000, 64, 64);
+const atmosphereMaterial = createAtmosphereMaterial();
+atmosphereMaterial.uniforms.glowColor.value.set(0x00b3ff);
+atmosphereMaterial.uniforms.coeficient.value = 0.9;
+atmosphereMaterial.uniforms.power.value = 2.5;
 
-var mesh = new THREE.Mesh(geometry, material );
-mesh.scale.multiplyScalar(1.005);
-containerEarth.add( mesh );
-// new THREEx.addAtmosphereMaterial2DatGui(material, datGUI)
+const atmosphereMesh = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+atmosphereMesh.scale.multiplyScalar(1.005);
+containerEarth.add(atmosphereMesh);
 
-var geometry = new THREE.SphereGeometry(3000, 64, 64);
-var material = createAtmosphereMaterial();
-material.side = THREE.BackSide;
-material.uniforms.glowColor.value.set(0x00b3ff);
-material.uniforms.coeficient.value = 0.5;
-material.uniforms.power.value	= 4.0
-var mesh = new THREE.Mesh(geometry, material );
-mesh.scale.multiplyScalar(1.05);
-containerEarth.add( mesh );
-// new THREEx.addAtmosphereMaterial2DatGui(material, datGUI)
+const atmosphereGlowGeometry = new THREE.SphereGeometry(3000, 64, 64);
+const atmosphereGlowMaterial = createAtmosphereMaterial();
+atmosphereGlowMaterial.side = THREE.BackSide;
+atmosphereGlowMaterial.uniforms.glowColor.value.set(0x00b3ff);
+atmosphereGlowMaterial.uniforms.coeficient.value = 0.5;
+atmosphereGlowMaterial.uniforms.power.value	= 5.0;
+
+const atmosphereGlowMesh = new THREE.Mesh(atmosphereGlowGeometry, atmosphereGlowMaterial);
+atmosphereGlowMesh.scale.multiplyScalar(1.05);
+containerEarth.add(atmosphereGlowMesh);
 
 export const earth$ = Rx.Observable.of(containerEarth);
 
@@ -103,7 +102,7 @@ createGui('containerEarth rotation x', 0, 0, 360)
     containerEarth.rotation.x = getRad(rotation);
   });
 
-addRenderer((scene, camare, delta) => {
+addRenderer((scene, camara, delta) => {
   containerEarth.rotation.x += getRad(0.3) * delta;
   earthCloudsMesh.rotation.y -= getRad(0.15) * delta;
 });
@@ -111,27 +110,25 @@ addRenderer((scene, camare, delta) => {
 const sunLight = new THREE.DirectionalLight(0xffddee, 0.65);
 sunLight.position.set(0, 200, 6000);
 sunLight.target.position.set(0, -3400, -6000);
-scene.add(sunLight);
 sunLight.castShadow = true;
 sunLight.shadow.bias = 0.001;
 // sunLight.shadow.Darkness = 0.2; ?? removed
 sunLight.shadow.mapSize.Width = 1024;
 sunLight.shadow.mapSize.Height = 1024;
+scene.add(sunLight);
 
-// const starField = THREEx.Planets.createStarfield(19900);
-var texture	= loader.load('dist/res/models/planets/starfield/star-environment-darker3.png');
-var material	= new THREE.MeshBasicMaterial({
-  map	: texture,
+const starFieldGeometry = new THREE.SphereGeometry(19900, 32, 32);
+const starFieldMaterial	= new THREE.MeshBasicMaterial({
+  map	: loader.load('dist/res/models/planets/starfield/star-environment-darker3.png'),
   side : THREE.BackSide
 });
-var geometry = new THREE.SphereGeometry(19900, 32, 32);
-var starField	= new THREE.Mesh(geometry, material);
-starField.rotation.x = getRad(-149);
-starField.rotation.y = getRad(-235);
-starField.rotation.z = getRad(139);
+const starFieldMesh	= new THREE.Mesh(starFieldGeometry, starFieldMaterial);
+starFieldMesh.rotation.x = getRad(-149);
+starFieldMesh.rotation.y = getRad(-235);
+starFieldMesh.rotation.z = getRad(139);
 
 rendererToggle$
   .startWith({})
   .subscribe(({ webRtcVideo }) =>
-    scene[!webRtcVideo ? 'add' : 'remove'](starField)
+    scene[webRtcVideo ? 'remove' : 'add'](starFieldMesh)
   );
